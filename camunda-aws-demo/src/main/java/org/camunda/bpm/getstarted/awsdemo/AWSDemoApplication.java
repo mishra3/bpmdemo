3b165e641a773e514c5c6006acab44ef9819d24d
soumyakantamishra <soumyakanta.mishra@se2.com>
2021-02-14 20:51:30 -0500

    First Commit

---
+++ camunda-aws-demo/src/main/java/org/camunda/bpm/getstarted/awsdemo/AWSDemoApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.getstarted.awsdemo;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("AWS Demo App")
+public class AWSDemoApplication extends ServletProcessApplication {
+  // empty implementation
+}
